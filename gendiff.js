e51e8a9249e9b2e8c6550806c1362751963e34b8
Eva Ya <pakalkaeva@gmail.com>
2024-05-04 13:15:18 +0800

    add -f  --format [type]", "output format")

--- gendiff.js
+++ gendiff.js
@@ -7,5 +7,6 @@ const program = new Command();
 program
   .description("Compares two configuration files and shows a difference.")
   .version("1.0")
-  .arguments("filepath1 filepath2");
+  .arguments("filepath1 filepath2")
+  .option("-f, --format [type]", "output format");
 program.parse();
